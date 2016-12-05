d43c991a345c6f67ac662cf5130c154067e79a12
Tom Scott <tscott@weblinc.com>
2016-12-05 15:42:45 -0500

    Add build to Rake task

--- lib/email_octopus/model.rb
+++ lib/email_octopus/model.rb
@@ -1,40 +1,59 @@
 # frozen_string_literal: true
 require 'email_octopus/api'
+require 'active_model/naming'
 
 module EmailOctopus
   # Common code for model objects.
   class Model
+    extend ActiveModel::Naming
+
+    # @!attribute attributes [r]
+    #   @return [Hash] Attributes related to this model.
     attr_reader :attributes
 
+    # @param params [Hash] Initial attributes for this model.
     def initialize(params = {})
       @attributes = params
       @api = API.new(EmailOctopus.config.api_key)
     end
 
+    # Instantiate a new resource and immediately persist it on the API.
+    #
+    # @param params [Hash] Initial attributes to save.
     def self.create(params = {})
       new(params).tap(&:save)
     end
 
+    # Find a resource by its given ID.
+    #
+    # @param id [String] ID provided by EmailOctopus for this resource.
     def self.find(id)
       new(id: id).tap(&:persisted?)
     end
 
+    # Start a new query on a group of model resources.
+    #
+    # @return [Query] for this model class.
     def self.all
       Query.new self
     end
 
-    def self.limit(num)
-      all.limit(num)
+    # Delegate all class method calls that aren't defined on the model
+    # class to its +Query+.
+    #
+    # @param method [Symbol] Name of the method to call.
+    # @param arguments [Array] Arguments to pass to the method.
+    # @throws [NoMethodError] when method not defined on +all+
+    # @return [Query]
+    def self.method_missing(method, *arguments)
+      return super unless respond_to? method
+      all.public_send method, *arguments
     end
 
-    def self.page(num)
-      all.page(num)
-    end
-
-    def self.resource_name
-      name.downcase + 's'
-    end
-
+    # Define a new attribute method on the model that reads from the
+    # +attributes+ hash.
+    #
+    # @param name [Symbol] Name of the attribute
     def self.attribute(name)
       define_method name do
         attributes[name.to_s]
@@ -45,12 +64,14 @@ module EmailOctopus
       end
     end
 
+    # 
     def persisted?
       id.present? && reload!
     end
 
+    # Run validations and persist to the database.
     def save
-      create || update
+      valid? && persist!
     end
 
     def as_json
@@ -81,8 +102,12 @@ module EmailOctopus
       @api.patch(url, to_json).success?
     end
 
+    def persist!
+      create || update
+    end
+
     def base_path
-      "/#{self.class.resource_name}"
+      "/#{model_name.collection}"
     end
 
     def path
